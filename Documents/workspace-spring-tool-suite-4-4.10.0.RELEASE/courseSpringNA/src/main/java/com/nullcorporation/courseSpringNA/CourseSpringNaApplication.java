86cbdb763f58003c578dddd951bed5aef9a4d17d
Thalis Silveira <thalissilbez7@gmail.com>
2021-08-18 15:50:27 -0300

    Project created

---
+++ Documents/workspace-spring-tool-suite-4-4.10.0.RELEASE/courseSpringNA/src/main/java/com/nullcorporation/courseSpringNA/CourseSpringNaApplication.java
@@ -0,0 +1,13 @@
+package com.nullcorporation.courseSpringNA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseSpringNaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseSpringNaApplication.class, args);
+	}
+
+}
---
+++ Documents/workspace-spring-tool-suite-4-4.10.0.RELEASE/courseSpringNA/src/test/java/com/nullcorporation/courseSpringNA/CourseSpringNaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nullcorporation.courseSpringNA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseSpringNaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
