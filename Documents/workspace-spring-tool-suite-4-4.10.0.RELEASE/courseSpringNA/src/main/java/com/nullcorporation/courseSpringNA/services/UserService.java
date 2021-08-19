5e034fbf87132e20abb93ac22cd283652604dd7d
Thalis Silveira <thalissilbez7@gmail.com>
2021-08-19 19:20:14 -0300

    Service layer, component registration

---
+++ Documents/workspace-spring-tool-suite-4-4.10.0.RELEASE/courseSpringNA/src/main/java/com/nullcorporation/courseSpringNA/services/UserService.java
@@ -0,0 +1,25 @@
+package com.nullcorporation.courseSpringNA.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.nullcorporation.courseSpringNA.entities.User;
+import com.nullcorporation.courseSpringNA.repositories.UserRepository;
+
+@Service
+public class UserService {
+	@Autowired
+	private UserRepository repository;
+
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+	
+	public User findbyId(Long id) {
+		Optional <User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
