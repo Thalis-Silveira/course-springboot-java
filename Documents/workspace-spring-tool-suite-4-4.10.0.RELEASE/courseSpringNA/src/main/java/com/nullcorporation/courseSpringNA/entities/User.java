710b35e8e16e33e684c1f6eb44bb62d4241ae2bc
Thalis Silveira <thalissilbez7@gmail.com>
2021-08-20 16:25:14 -0300

    Order, Instant, ISO 8601(Part 1)

--- Documents/workspace-spring-tool-suite-4-4.10.0.RELEASE/courseSpringNA/src/main/java/com/nullcorporation/courseSpringNA/entities/User.java
+++ Documents/workspace-spring-tool-suite-4-4.10.0.RELEASE/courseSpringNA/src/main/java/com/nullcorporation/courseSpringNA/entities/User.java
@@ -1,32 +1,53 @@
 package com.nullcorporation.courseSpringNA.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 
-@Data
 @AllArgsConstructor
 @NoArgsConstructor
+@ToString(exclude="id")//checar se o password vai aqui tambem
 
 @Entity
+@Table(name = "tb_user")
 public class User implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
-	private String name;
-	private String email;
-	private String phone;
-	private String password;
+	@Getter@Setter private Long id;
+	@Getter@Setter private String name;
+	@Getter@Setter private String email;
+	@Getter@Setter private String phone;
+	@Getter@Setter private String password;
+	
+	@OneToMany(mappedBy = "client")
+	@Getter List<Order>orders = new ArrayList<>();
+	
+	
+	public User(Long id, String name, String email, String phone, String password) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.phone = phone;
+		this.password = password;
+	}
+	
 	
 	@Override
 	public boolean equals(Object obj) {
@@ -51,6 +72,5 @@ public class User implements Serializable{
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		return result;
 	}
-	
 	
 }
