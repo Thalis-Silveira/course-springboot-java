d81c6be3c9c8eca9aa1ed69e7f52e277b58dec05
Thalis Silveira <thalissilbez7@gmail.com>
2021-08-20 18:23:12 -0300

    OrderStatus enum

--- Documents/workspace-spring-tool-suite-4-4.10.0.RELEASE/courseSpringNA/src/main/java/com/nullcorporation/courseSpringNA/entities/Order.java
+++ Documents/workspace-spring-tool-suite-4-4.10.0.RELEASE/courseSpringNA/src/main/java/com/nullcorporation/courseSpringNA/entities/Order.java
@@ -12,15 +12,13 @@ import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.nullcorporation.courseSpringNA.entities.enums.OrderStatus;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Getter
-@Setter
-@AllArgsConstructor
+
 @NoArgsConstructor
 @Entity
 @Table(name = "tb_order")
@@ -29,15 +27,27 @@ public class Order implements Serializable {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
+	@Getter @Setter private Long id;
 
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
-	private Instant moment;
+	@Getter @Setter private Instant moment;
+	
+	private Integer orderStatus;
 
 	@ManyToOne
 	@JoinColumn(name = "client_id")
 	private User client;
 
+	
+	public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
+		super();
+		this.id = id;
+		this.moment = moment;
+		setOrderStatus(orderStatus);
+		this.client = client;
+	}
+	
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -62,5 +72,18 @@ public class Order implements Serializable {
 			return false;
 		return true;
 	}
+
+
+	public OrderStatus getOrderStatus() {
+		return OrderStatus.valueOf(orderStatus);
+	}
+
+
+	public void setOrderStatus(OrderStatus orderStatus) {
+		if(orderStatus != null) {
+			this.orderStatus = orderStatus.getCode();
+		}
+	}
+
 
 }
