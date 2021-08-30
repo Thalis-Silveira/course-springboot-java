5c459503b2a93073c23717472530014fe573819f
Thalis Silveira <thalissilbez7@gmail.com>
2021-08-30 20:16:29 -0300

    Exception handling - findById

--- Documents/workspace-spring-tool-suite-4-4.10.0.RELEASE/courseSpringNA/src/main/java/com/nullcorporation/courseSpringNA/services/UserService.java
+++ Documents/workspace-spring-tool-suite-4-4.10.0.RELEASE/courseSpringNA/src/main/java/com/nullcorporation/courseSpringNA/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.nullcorporation.courseSpringNA.entities.User;
 import com.nullcorporation.courseSpringNA.repositories.UserRepository;
+import com.nullcorporation.courseSpringNA.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -20,7 +21,7 @@ public class UserService {
 	
 	public User findbyId(Long id) {
 		Optional <User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj){
